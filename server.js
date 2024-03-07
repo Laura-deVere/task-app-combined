ce0593bc7e4eaf34bb37469f95d83b355f04e410
Laura deVere-Hopkins <lauradevh@gmail.com>
2024-03-06 19:45:42 -0500

    added public folder to server

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ import morgan from "morgan";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 
+// public
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
 // Routes
 import projectsRouter from "./routes/projectsRouter.js";
 import authRouter from "./routes/authRouter.js";
@@ -17,9 +22,13 @@ import userRouter from "./routes/userRouter.js";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
+
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -27,6 +36,10 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1", authenticateUser, projectsRouter);
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
 
 app.use("*", (req, res) => {
 	// access resource we don't have
