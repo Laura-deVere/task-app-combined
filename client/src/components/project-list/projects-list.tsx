c6b16f6e847529f894d9fdfab3e6f21546b95f06
Laura deVere-Hopkins <lauradevh@gmail.com>
2024-03-21 18:34:46 -0400

    switched to using dnd-kit for drag and drop

--- client/src/components/project-list/projects-list.tsx
+++ client/src/components/project-list/projects-list.tsx
@@ -1,13 +1,25 @@
-import update from "immutability-helper";
-import { useCallback, useContext, useRef, useEffect } from "react";
+import { useCallback, useContext, useRef, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+
+import {
+	DndContext,
+	closestCenter,
+	DragOverlay,
+	KeyboardSensor,
+	PointerSensor,
+	useSensor,
+	useSensors,
+} from "@dnd-kit/core";
+import {
+	arrayMove,
+	SortableContext,
+	sortableKeyboardCoordinates,
+	verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 import ProjectCard from "../project-card/project-card";
 import { Project } from "../../types";
 import { ProjectsContext } from "../../context/projects-context";
-import CustomDragLayer from "../../components/custom-drag-layer/custom-drag-layer";
 
 import "./projects-list.scss";
 
@@ -15,55 +27,86 @@ const className = "projects-list";
 
 const ProjectsList: React.FC = () => {
 	const gridRef = useRef(null);
+
+	const [activeId, setActiveId] = useState<Project | null>(null);
 	const { projects, setProjects } = useContext(ProjectsContext);
+
+	const sensors = useSensors(
+		useSensor(PointerSensor),
+		useSensor(KeyboardSensor, {
+			coordinateGetter: sortableKeyboardCoordinates,
+		})
+	);
 
 	useEffect(() => {
 		const grid = gridRef.current;
 		if (grid) adjustGridItemsHeight(grid);
 	});
 
-	const moveProject = useCallback(
-		(dragIndex: number, hoverIndex: number) => {
-			console.log("moveProject", {
-				dragIndex: projects[dragIndex].name,
-				hoverIndex: projects[hoverIndex].name,
-			});
-			// const newProjectsArray = [...projects];
-			// const dragProject = newProjectsArray[dragIndex];
-			// newProjectsArray.splice(dragIndex, 1);
-			// newProjectsArray.splice(hoverIndex, 0, dragProject);
-			// setProjects(newProjectsArray);
-			const newProjectsArray = update(projects, {
-				$splice: [
-					[dragIndex, 1],
-					[hoverIndex, 0, projects[dragIndex]],
-				],
-			});
-			setProjects(newProjectsArray);
+	const handleDragEnd = useCallback(
+		(event) => {
+			const { active, over } = event;
+			console.log(active.id, over.id);
+
+			if (active.id !== over.id) {
+				const oldIndex = projects.findIndex(
+					(project: Project) => project._id === active.id
+				);
+				const newIndex = projects.findIndex(
+					(project: Project) => project._id === over.id
+				);
+				const moveProject = arrayMove(projects, oldIndex, newIndex);
+				setProjects(moveProject);
+			}
+
+			setActiveId(null);
 		},
 		[projects]
 	);
 
+	function handleDragStart(event) {
+		const { active } = event;
+
+		setActiveId(projects.find((project: Project) => project._id === active.id));
+	}
+
 	return (
-		<DndProvider backend={HTML5Backend}>
-			<CustomDragLayer />
-			<ul className={className} ref={gridRef}>
-				{projects.map((project: Project, index: number) => {
-					const { _id, name, tasks } = project;
-					return (
-						<ProjectCard
-							key={_id}
-							project={project}
-							name={name}
-							tasks={tasks}
-							moveProject={moveProject}
-							index={index}
-							_id={_id}
-						/>
-					);
-				})}
-			</ul>
-		</DndProvider>
+		<DndContext
+			sensors={sensors}
+			collisionDetection={closestCenter}
+			onDragEnd={handleDragEnd}
+			onDragStart={handleDragStart}
+		>
+			<SortableContext items={projects} strategy={verticalListSortingStrategy}>
+				<ul className={className} ref={gridRef}>
+					{projects.map((project: Project, index: number) => {
+						const { _id, name, tasks } = project;
+						return (
+							<ProjectCard
+								key={_id}
+								project={project}
+								name={name}
+								tasks={tasks}
+								index={index}
+								_id={_id}
+							/>
+						);
+					})}
+				</ul>
+			</SortableContext>
+			<DragOverlay>
+				{activeId ? (
+					<ProjectCard
+						key={activeId._id}
+						project={activeId}
+						name={activeId.name}
+						tasks={activeId.tasks}
+						index={-1}
+						_id={activeId._id}
+					/>
+				) : null}
+			</DragOverlay>
+		</DndContext>
 	);
 };
 
@@ -74,7 +117,7 @@ const adjustGridItemsHeight = (grid: HTMLElement) => {
 	for (let i = 0; i < items.length; i++) {
 		// let item = items[i];
 		let item = ReactDOM.findDOMNode(items[i])! as HTMLElement;
-		console.log(typeof item);
+
 		let rowHeight = parseInt(
 			window.getComputedStyle(grid).getPropertyValue("grid-auto-rows")
 		);
